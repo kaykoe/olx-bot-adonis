659ebce95f200591520b99d1a00b6a795b8c6860
kaykoe <karolkoenigg@gmail.com>
2025-04-06 18:48:03 +0200

    feat: store search query

--- app/controllers/search_queries_controller.ts
+++ app/controllers/search_queries_controller.ts
@@ -1,7 +1,12 @@
 import type { HttpContext } from "@adonisjs/core/http";
 
+import SearchQuery from "#models/search_query";
+import { createSearchQueryValidator } from "#validators/search_query";
+
 export default class SearchQueriesController {
-  async store({ request }: HttpContext) {
-    return request.all();
+  async store({ request, response }: HttpContext) {
+    const data = await request.validateUsing(createSearchQueryValidator);
+    const searchQuery = await SearchQuery.create(data);
+    return response.created(searchQuery);
   }
 }
