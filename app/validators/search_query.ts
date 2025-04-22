78a764a4336186be6c7867700cea5cfde7f1f070
kaykoe <karolkoenigg@gmail.com>
2025-04-22 23:43:39 +0200

    feat: add updateSearchQueryValidator

--- app/validators/search_query.ts
+++ app/validators/search_query.ts
@@ -4,23 +4,22 @@ import { FieldContext } from "@vinejs/vine/types";
 const maxRule = vine.createRule(
   (value: unknown, _options: undefined, field: FieldContext) => {
     // When a value is null or undefined the rule will not be called, so no need to check value for that
-    // This rule is called after .number() so value should be a number
+    // This rule is called after .number() so value is a valid number
     const maxValue = vine.helpers.asNumber(value);
 
     const maxFieldName = field.name as string;
     const minFieldName = maxFieldName.replace("Max", "Min");
-    const parent = field.parent as Record<string, unknown>;
-    // .asNumber() will return NaN if the value is not a number
-    // If it is NaN, maxValue is always valid
+    const parent = field.parent as Record<string, FieldContext>;
     const minValue = vine.helpers.asNumber(parent[minFieldName]);
-
-    // Check if minValue is a valid number (not NaN) before comparing
-    if (!Number.isNaN(minValue) && maxValue < minValue) {
-      return field.report(
-        `{${maxFieldName}} must be greater than or equal to {${minFieldName}}`,
-        "validation_error",
-        field,
-      );
+    // If minValue is NaN, maxValue is always valid
+    if (!Number.isNaN(minValue)) {
+      if (maxValue < minValue) {
+        return field.report(
+          `{${maxFieldName}} must be greater than or equal to {${minFieldName}}`,
+          "validation_error",
+          field,
+        );
+      }
     }
   },
 );
@@ -29,6 +28,20 @@ export const createSearchQueryValidator = vine.compile(
   vine.object({
     name: vine.string().trim(),
     locationId: vine.number(),
+
+    priceMin: vine.number().positive().optional(),
+    priceMax: vine.number().positive().optional().use(maxRule()),
+    sizeMin: vine.number().positive().optional(),
+    sizeMax: vine.number().positive().optional().use(maxRule()),
+    roomsMin: vine.number().positive().optional(),
+    roomsMax: vine.number().positive().optional().use(maxRule()),
+  }),
+);
+
+export const updateSearchQueryValidator = vine.compile(
+  vine.object({
+    name: vine.string().trim().optional(),
+    locationId: vine.number().optional(),
 
     priceMin: vine.number().positive().optional(),
     priceMax: vine.number().positive().optional().use(maxRule()),
